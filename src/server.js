36c64ef2233129611c971b4392b59b70253bdbb9
LuizzP <luizz.paullo@gmail.com>
2021-04-02 06:45:21 -0400

    organizando a estrutura de pastas

--- src/server.js
+++ src/server.js
@@ -2,14 +2,24 @@
  * CONFIG SERVIDOR COM EXPRESS
  * -> npm init -y
  * -> npm install express
- * [ ] Criar pasta e arquivo /src/server.js
+ * [ ] Criar pasta /src
+ *                      /server.js
+ *                      /views/  todos os html aqui
+ * 
  **/
 
 const express = require('express')
 const server = express()
 
+
+/**
+ * Habilitar arquivos Statics
+ */
+server.use(express.static('public'))
+
 server.get('/',(req, res )=> {
-    console.log('Entrei no index')
+    // console.log(__dirname)
+    return res.sendFile(__dirname + '/views/index.html')
 })
 
 server.listen(3000, () => console.log('Servidor listening on port 3000'))
