4a607e00c1b27a09798b32f8f8f46b4057a4a393
LuizzP <luizz.paullo@gmail.com>
2021-04-02 07:39:40 -0400

    install template engine ejs

--- src/server.js
+++ src/server.js
@@ -5,11 +5,30 @@
  * [ ] Criar pasta /src
  *                      /server.js
  *                      /views/  todos os html aqui
+ * ->npm i nodemon -D 
+ * 
+ * Editar o package.json 
+ *  "main": "src/server.js"
+ *  "scripts" : {
+ *      "dev":"nodemon ."}
+ * 
+ *  Para Executar o <Servidor>
+ * 
+ * -> npm run dev
+ * 
+ * Organizar estrutura do projetos
+ *  criar pasta public na raiz 
+ *                 /public/
+ *                      public/images/
+ *                      public/scripts/
+ *                      public/styles
+ *  # Criar arquivos routes.js na src
  * 
  **/
 
 const express = require('express')
 const server = express()
+const routes = require('./routes')
 
 
 /**
@@ -17,9 +36,7 @@ const server = express()
  */
 server.use(express.static('public'))
 
-server.get('/',(req, res )=> {
-    // console.log(__dirname)
-    return res.sendFile(__dirname + '/views/index.html')
-})
+//Routes
+server.use(routes)
 
 server.listen(3000, () => console.log('Servidor listening on port 3000'))
