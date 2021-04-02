724da57e5d9d21c044bf72949904cb0fc09a3388
LuizzP <luizz.paullo@gmail.com>
2021-04-02 05:41:21 -0400

    process

---
+++ src/server.js
@@ -0,0 +1,12 @@
+/** 
+ * CONFIG SERVIDOR COM EXPRESS
+ * -> npm init -y
+ * -> npm install express
+ * [ ] Criar pasta e arquivo /src/server.js
+ **/
+
+const express = require('express')
+const server = express()
+
+console.log(server)
+
