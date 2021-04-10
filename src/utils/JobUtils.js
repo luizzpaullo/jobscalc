850b0a801848e2c555ab20b62a43ca8f6519a288
LuizzP <luizz.paullo@gmail.com>
2021-04-10 00:26:17 -0400

    correcao bug math.ceil

--- src/utils/JobUtils.js
+++ src/utils/JobUtils.js
@@ -10,7 +10,7 @@ module.exports = {
         const timeDiffInMs = dueDateInMs - Date.now()
         //transformar milissegundos em dias
         const dayInMs = 1000 * 60 * 60 * 24
-        const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+        const dayDiff = Math.ceil(timeDiffInMs / dayInMs)
 
         //restam X dias
         return dayDiff
